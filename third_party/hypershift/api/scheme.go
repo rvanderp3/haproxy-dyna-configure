cc4b6aaea0230b7f202fa234217d1c27f1776e4b
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2023-06-09 15:41:45 -0400

    introduce hypershift types

---
+++ third_party/hypershift/api/scheme.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"os"
+
+	hyperv1beta1 "github.com/rvanderp3/haproxy-dyna-configure/third_party/hypershift/api/v1beta1"
+)
+
+var (
+	Scheme = runtime.NewScheme()
+	// TODO: Even though an object typer is specified here, serialized objects
+	// are not always getting their TypeMeta set unless explicitly initialized
+	// on the variable declarations.
+	// Investigate https://github.com/kubernetes/cli-runtime/blob/master/pkg/printers/typesetter.go
+	// as a possible solution.
+	// See also: https://github.com/openshift/hive/blob/master/contrib/pkg/createcluster/create.go#L937-L954
+	YamlSerializer = json.NewSerializerWithOptions(
+		json.DefaultMetaFactory, Scheme, Scheme,
+		json.SerializerOptions{Yaml: true, Pretty: true, Strict: true},
+	)
+	JsonSerializer = json.NewSerializerWithOptions(
+		json.DefaultMetaFactory, Scheme, Scheme,
+		json.SerializerOptions{Yaml: false, Pretty: true, Strict: true},
+	)
+)
+
+func init() {
+
+	hyperv1beta1.AddToScheme(Scheme)
+}
---
+++ third_party/hypershift/api/v1beta1/README.md
@@ -0,0 +1,3 @@
+# Overview
+
+Due to conflicting dependencies with hypershift, the API types are being mirrored.  We only need the APIs from the hypershift project.
